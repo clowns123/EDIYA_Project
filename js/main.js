598f029bbba257345ee5123d42650ea18abbf5bf
clowns123 <clonws1232@gmail.comm>
2020-04-06 19:03:03 +0900

    header javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var header = document.querySelector('.header');
+var menuNav = header.querySelector('.menuNav');
+var openButton = header.querySelector('.menu__open');
+var closeButton = header.querySelector('.menu__close');
+
+function openClick(){
+
+}
+function closeClick(){
+    
+}
